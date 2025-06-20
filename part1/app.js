cba9f4ac395c8c608b91d477393449fc20aec3d8
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:30:04 +0000

    [automatic commit] 2025-06-20 13:30:04 UTC

--- part1/app.js
+++ part1/app.js
@@ -62,8 +62,8 @@ app.get('/api/walkers/summary', async (req, res) => {
             (
             SELECT COUNT(*) FROM WalkRuquests w
             JOIN WalkApplications a ON a.request_id = w.request_id AND a.walker_id = u.user_id
-            WHERE w.staus = 'compl'
-            )
+            WHERE w.staus = 'completed'
+            )AS completed
             `
         );
         res.json(rows);

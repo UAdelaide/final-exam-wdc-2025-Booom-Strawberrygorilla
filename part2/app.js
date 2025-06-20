4e96adb64f5c71b61db6840ab55119876e557488
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 17:55:21 +0000

    [automatic commit] 2025-06-20 17:55:21 UTC

--- part2/app.js
+++ part2/app.js
@@ -12,15 +12,16 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/walks', walkRoutes);
-app.use('/api/users', userRoutes);
-
 const session = require('express-session');
 app.use(session({
   secret: 'dogsecret',
   resave: false,
   saveUninitialized: true,
 }));
+
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
+
 
 // Export the app instead of listening here
 module.exports = app;

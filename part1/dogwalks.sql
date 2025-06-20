224f19868d997e82c3002cb05c41ac8bb52659b4
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 10:28:35 +0000

    [automatic commit] 2025-06-20 10:28:35 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -63,8 +63,8 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id from Users where username = 'alice123'), 'Max', 'medium'),
-((SELECT user_id from Users where username = 'carol123'), 'Bella', 'small')，
-((SELECT user_id from Users where username = 'bobwalke'), 'Ruby', 'medium')，
-((SELECT user_id from Users where username = 'davidwalker'), 'Alex', 'small')，
+((SELECT user_id from Users where username = 'carol123'), 'Bella', 'small'),
+((SELECT user_id from Users where username = 'bobwalke'), 'Ruby', 'medium'),
+((SELECT user_id from Users where username = 'davidwalker'), 'Alex', 'small'),
 ((SELECT user_id from Users where username = 'ellieowner'), 'Angle', 'medium');
 

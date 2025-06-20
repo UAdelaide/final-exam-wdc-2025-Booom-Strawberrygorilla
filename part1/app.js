b1230c28e8e8301350d86affb1a98279c8b9b347
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:17:35 +0000

    [automatic commit] 2025-06-20 13:17:35 UTC

--- part1/app.js
+++ part1/app.js
@@ -32,7 +32,21 @@ app.get('/api/dogs', async (req, res) => {
         );
         res.json(rows);
     } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch books' });
+    res.status(500).json({ error: 'Failed to fetch dogs' });
+  }
+});
+
+// 
+app.get('/api/dogs', async (req, res) => {
+    try{
+        const [rows] = await db.execute(
+            `SELECT d.name AS dog_name, d.size, u.username AS owner_username
+            FROM Dogs d
+            JOIN Users u ON d.owner_id = u.user_id`
+        );
+        res.json(rows);
+    } catch (err) {
+    res.status(500).json({ error: 'Failed to fetch dogs' });
   }
 });
 

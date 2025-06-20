ec9aa82be699332f92c444b3ddb7d2898c6b7e57
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:15:47 +0000

    [automatic commit] 2025-06-20 13:15:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -29,7 +29,8 @@ app.get{'/api/dogs', asyns (req, res) => {
             `SELECT d.name AS dog_name, d.size, u.username AS owner_username
             FROM Dogs d
             JOIN Users u ON d.owner_id = u.user_id`
-        )
+        );
+        res.json()
     }
 }
 

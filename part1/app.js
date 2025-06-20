5c4887b3e4fe84f19b3b75a15192f2ec7a216961
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:18:37 +0000

    [automatic commit] 2025-06-20 13:18:37 UTC

--- part1/app.js
+++ part1/app.js
@@ -40,13 +40,11 @@ app.get('/api/dogs', async (req, res) => {
 app.get('/api/walkrequests/open', async (req, res) => {
     try{
         const [rows] = await db.execute(
-            `SELECT d.name AS dog_name, d.size, u.username AS owner_username
-            FROM Dogs d
-            JOIN Users u ON d.owner_id = u.user_id`
+            `
         );
         res.json(rows);
     } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch walk' });
+    res.status(500).json({ error: 'Failed to fetch walkrequests' });
   }
 });
 

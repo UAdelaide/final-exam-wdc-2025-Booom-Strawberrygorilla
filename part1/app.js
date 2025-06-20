78dd89bef70b592f284acd381b2796af14a3ac3b
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:25:57 +0000

    [automatic commit] 2025-06-20 13:25:57 UTC

--- part1/app.js
+++ part1/app.js
@@ -57,7 +57,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/walkers/summary', async (req, res) => {
     try{
         const [rows] = await db.execute(
-            `SELECT 
+            `SELECT u.username AS walker_username,
+            
             `
         );
         res.json(rows);

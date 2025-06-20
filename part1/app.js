942cfc9cbc41457fddb5fa4ab16b81c23619875d
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:55:39 +0000

    [automatic commit] 2025-06-20 13:55:38 UTC

--- part1/app.js
+++ part1/app.js
@@ -88,5 +88,5 @@ app.get('/api/walkers/summary', async (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+    console.log('Server running on http://localhost:3000');
 });

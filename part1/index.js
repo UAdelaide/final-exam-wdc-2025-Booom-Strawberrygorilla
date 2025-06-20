f87814e2d7069e08f9300003695d14027f4803b2
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 14:42:24 +0000

    [automatic commit] 2025-06-20 14:42:24 UTC

--- part1/index.js
+++ part1/index.js
@@ -9,7 +9,8 @@ CreateApp({
             const res = await fetch('https://images.dog.ceo/breeds/mudhol-indian/Indian-Mudhol.jpg');
             const data = aeait res.json();
             dogImage.value = data.message;
-        } 
+        } catch (error) {
+        console.error('Failed to fetch dog', error);
     }
 )
 

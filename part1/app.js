8ed5d564810dd9252af44d31838fed26b95655d1
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:31:53 +0000

    [automatic commit] 2025-06-20 13:31:53 UTC

--- part1/app.js
+++ part1/app.js
@@ -67,6 +67,7 @@ app.get('/api/walkers/summary', async (req, res) => {
             FROM Users u
             LEFT JOIN WalkRatings r ON u.user_id = r.walker_id
             WHERE u.role = 'walker'
+            GROUP BY u.user_id
             `
         );
         res.json(rows);

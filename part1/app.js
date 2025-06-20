fc5c52d8ea365001a2577a1ee2a8d59779371aa9
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:34:18 +0000

    [automatic commit] 2025-06-20 13:34:18 UTC

--- part1/app.js
+++ part1/app.js
@@ -44,7 +44,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
             FROM WalkRequests r
             JOIN Dogs d ON r.dog_id = d.dog_id
             JOIN Users u ON d.owner_id = u.user_id
-            WHERE r.staus = 'open'
+            WHERE r.status = 'open'
             `
         );
         res.json(rows);

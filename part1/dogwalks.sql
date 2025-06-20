2f94d0e1e7ed37cfdf3a83e77c16ec2265763978
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 10:29:58 +0000

    [automatic commit] 2025-06-20 10:29:57 UTC

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -69,3 +69,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id from Users where username = 'ellieowner'), 'Angle', 'medium');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+((SELECT dog_id from Users where username = 'alice123'), 'Max', 'medium'),

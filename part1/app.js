cd35bcd96e86945a89d1cbbe14131d5e058d5d45
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 13:47:45 +0000

    [automatic commit] 2025-06-20 13:47:44 UTC

--- part1/app.js
+++ part1/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var mysql = require('mysql2/promise');
-const fs = require('fs');
 var app = express();
 
 app.use(express.json());
@@ -17,7 +16,7 @@ let db;
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
 
 

17e58a204ddb766826de7ef7d868dc862aba6843
Booom-Strawberrygorilla <a1914744@adelaide.edu.au>
2025-06-20 20:42:07 +0000

    [automatic commit] 2025-06-20 20:42:07 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -79,7 +79,9 @@ router.get('/mine/dogs', async(req, res)=>{
     const [rows] =await db.query(
       'SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
       [req.session.user.user_id]
-    )};
+    );
+    res.json
+  }
 }
 )
 
